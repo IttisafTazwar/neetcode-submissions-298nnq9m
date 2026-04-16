0d12082aa63547cf990c0265683c356384c01114
Syed Ittisaf Tazwar <57845773+IttisafTazwar@users.noreply.github.com>
2026-04-17 03:27:06 +1000

    Add: two-integer-sum-ii - submission-4

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-4.js
@@ -0,0 +1,16 @@
+class Solution {
+    /**
+     * @param {number[]} numbers
+     * @param {number} target
+     * @return {number[]}
+     */
+    twoSum(numbers, target) {
+        for(let i=0; i<numbers.length;i++){
+            let rem= target- numbers[i]
+            if(numbers.includes(rem)){
+                let index= numbers.indexOf(rem)
+                return [i+1,index+1]
+            }
+        }
+    }
+}
